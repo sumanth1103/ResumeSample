abcb7b4e17e08e760e79c5929148a9a21ccd3d79
Reddipalli <899167@cognizant.com>
2021-04-28 20:00:37 +0530

    Add project files.

---
+++ ResumeSample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ResumeSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResumeSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Authorize]
+        public ActionResult Index()
+        {
+            //ViewBag.imagePath = "~/Views/Home/Choose.png";
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Selection()
+        {
+            return View();
+        }
+    }
+}
